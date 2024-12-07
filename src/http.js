ddbbf60a463c234af65cebb9b7f646c4359807b1
Rohit Singh <rohitsinghpb4@gmail.com>
2024-12-07 19:57:51 +0530

    api changed

--- src/http.js
+++ src/http.js
@@ -1,39 +1,39 @@
 export async function fetchAvailablePlaces() {
-    const response = await fetch("http://localhost:1001/places");
-    const resData = await response.json();
+  const response = await fetch("http://localhost:1000/places");
+  const resData = await response.json();
 
-    if (!response.ok) {
-        throw new Error("failed to fetch places");
-    }
+  if (!response.ok) {
+    throw new Error("failed to fetch places");
+  }
 
-    return resData.places;
+  return resData.places;
 }
 
 export async function fetchUserPlaces() {
-    const response = await fetch("http://localhost:1001/user-places");
-    const resData = await response.json();
+  const response = await fetch("http://localhost:1000/user-places");
+  const resData = await response.json();
 
-    if (!response.ok) {
-        throw new Error("Failed to fetch user places");
-    }
+  if (!response.ok) {
+    throw new Error("Failed to fetch user places");
+  }
 
-    return resData.places;
+  return resData.places;
 }
 
 export async function updateUserPlaces(places) {
-    const response = await fetch("http://localhost:1001/user-places", {
-        method: "Put",
-        body: JSON.stringify({ places }),
-        headers: {
-            "Content-Type": "application/json",
-        },
-    });
+  const response = await fetch("http://localhost:1000/user-places", {
+    method: "Put",
+    body: JSON.stringify({ places }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
-    const resData = await response.json();
+  const resData = await response.json();
 
-    if (!response.ok) {
-        throw new Error("Failed to update user data");
-    }
+  if (!response.ok) {
+    throw new Error("Failed to update user data");
+  }
 
-    return resData.message;
+  return resData.message;
 }
